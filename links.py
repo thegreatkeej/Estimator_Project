e353b97dec53bfa7004cc1d6707b5b0a90645d6f
thegreatkeej <106637080+thegreatkeej@users.noreply.github.com>
2022-09-28 15:15:32 -0700

    Day 0: Light research, pseudo code, game plan

---
+++ links.py
@@ -0,0 +1,11 @@
+# 1/2 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+https://www.homedepot.com/p/1-2-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-853428/100400405 
+
+# 3/4 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+https://www.homedepot.com/p/3-4-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-853429/100400406
+
+# 1 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+https://www.homedepot.com/p/1-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-101568/100400409
+
+# 2 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+https://www.homedepot.com/p/2-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-101592/100400411
