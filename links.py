39ae387d08239f1aa2513dbbc431bec94fff2a74
thegreatkeej <106637080+thegreatkeej@users.noreply.github.com>
2022-10-04 17:16:13 -0700

    initial research on Home Depot links and Key words for searches in Lowes, Grainger sites. PLEASE SEE PSEUDO CODE

--- links.py
+++ links.py
@@ -1,11 +1,52 @@
-# 1/2 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+# EMT (Item names work for Home Depot, Lowes, and Grainger)
+# 1/2 in. x 10 ft. Electric Metallic Tube (EMT) Conduit 
 https://www.homedepot.com/p/1-2-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-853428/100400405 
 
-# 3/4 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+# 3/4 in. x 10 ft. Electric Metallic Tube (EMT) Conduit 
 https://www.homedepot.com/p/3-4-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-853429/100400406
 
-# 1 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
+# 1 in. x 10 ft. Electric Metallic Tube (EMT) Conduit 
 https://www.homedepot.com/p/1-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-101568/100400409
 
-# 2 in. x 10 ft. Electric Metallic Tube (EMT) Conduit Link
-https://www.homedepot.com/p/2-in-x-10-ft-Electric-Metallic-Tube-EMT-Conduit-101592/100400411
+
+# EMT Fittings (Item names work for Home Depot (5-pack), Lowes (5-pack), and Grainger (per 1))
+# 1/2 in. emt set-screw connectors  (Item names work for Home Depot (5-pack), Lowes (5-pack), and Grainger (1-pack))
+https://www.homedepot.com/p/Halex-1-2-in-Electrical-Metallic-Tube-EMT-Set-Screw-Connectors-5-Pack-26270/100137321
+# zinc 1/2 in. emt set-screw coupling (Item names work for Home Depot (5-pack), Lowes (5-pack), and Grainger (per 1))
+# 5-pack is 4th item to right
+https://www.homedepot.com/p/RACO-1-2-in-EMT-Set-Screw-Coupling-5-Pack-2022B5/203671378
+
+# 3/4 in. emt set-screw connectors  (5-pack)
+https://www.homedepot.com/p/Halex-3-4-in-Electrical-Metallic-Tube-EMT-Set-Screw-Connector-5-Pack-23212/202077145
+# 3/4 in. emt set-screw couplings does not have a direct search for Home Depot but does work for Lowes (5-pack) and Grainger (1-pack)
+https://www.homedepot.com/p/Halex-3-4-in-Electric-Metallic-Tube-EMT-Set-Screw-Coupling-5-Pack-26282/202288508
+
+# 1 in. emt set-screw connector (Item names work for all (1-pack)
+# 1-pack is 3rd link to right
+https://www.homedepot.com/p/Halex-1-in-Electrical-Metallic-Tube-EMT-Set-Screw-Connector-12310/202077147
+# 1in. set-screw emt coupling - 1-pack is 3rd link to right (Lowes and Grainger require a different search name)
+https://www.homedepot.com/p/Halex-1-in-Electric-Metallic-Tube-EMT-Set-Screw-Coupling-62810/202241090
+
+
+# THHN (Item works for Home Depot and Lowes but grainger requires some extra steps)
+# 500 ft. #4 black stranded cu simpull thhn wire
+https://www.homedepot.com/p/Cerrowire-500-ft-4-Gauge-Black-Stranded-Copper-THHN-Wire-112-4401J/202304774
+
+# 500 ft. #6 gauge black stranded cu simpull thhn wire
+https://www.homedepot.com/p/Cerrowire-500-ft-6-Gauge-Black-Stranded-Copper-THHN-Wire-112-4201J/202304649
+
+# 500 ft. #10 gauge black stranded cu simpull thhn wire
+https://www.homedepot.com/p/Cerrowire-500-ft-10-Gauge-Black-Stranded-Copper-THHN-Wire-112-3871J/202304645
+# Note-> "THHN 4 AWG Black 500 ft Stranded wire" format works for grainger and Lowes but not  for Home Depot wire size #4
+
+
+# Romex (Works for all home depot but not Lowes or Grainger)
+# 250' 14/2 romex
+https://www.homedepot.com/p/Southwire-250-ft-14-2-Romex-SIMpull-Solid-NM-B-W-G-Wire-28827469/202019377
+# 250' 14/3 romex
+https://www.homedepot.com/p/Southwire-250-ft-14-3-Solid-Romex-SIMpull-CU-NM-B-W-G-Wire-63946855/202316275
+
+# 250' 12/2 romex
+https://www.homedepot.com/p/Southwire-250-ft-12-2-Solid-Romex-SIMpull-CU-NM-B-W-G-Wire-28828269/202019375
+# 250' 12/3 romex
+https://www.homedepot.com/p/Southwire-250-ft-12-3-Solid-Romex-SIMpull-CU-NM-B-W-G-Wire-63947655/202316276
