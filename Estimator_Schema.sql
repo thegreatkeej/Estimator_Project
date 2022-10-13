a69e5b76d78a1e07b45e6b631b41f467f3f9fe03
thegreatkeej <106637080+thegreatkeej@users.noreply.github.com>
2022-10-12 18:39:08 -0700

    Cleaned Home Depot script, added to coding examples(API, Web Scrape), edited Game Plan, created HD.csv and HD.xlsx

---
+++ Estimator_Schema.sql
@@ -0,0 +1,23 @@
+-- Home Depot (API 1)
+CREATE TABLE HomeDepot_table (
+  Material_Description VARCHAR (80) PRIMARY KEY NOT NULL,
+  Product_id TEXT,
+  Material_Price FLOAT,
+  Vendor_Location TEXT 
+);
+
+-- Lowes (API 2)
+CREATE TABLE Lowes_table (
+  Material_Description VARCHAR (80) PRIMARY KEY NOT NULL,
+  Product_id TEXT,
+  Material_Price FLOAT,
+  Vendor_Location TEXT 
+);
+
+-- Grainger (API 3)
+CREATE TABLE Lowes_table (
+  Material_Description VARCHAR (80) PRIMARY KEY NOT NULL,
+  Product_id TEXT,
+  Material_Price FLOAT,
+  Vendor_Location TEXT 
+);
