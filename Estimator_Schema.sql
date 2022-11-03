f4a15f8830146b8b6bb400f9413bfcb3bc7d3bd7
thegreatkeej <106637080+thegreatkeej@users.noreply.github.com>
2022-11-03 11:18:42 -0700

    Added schema, PGAdmin code, consolidated data (see resources) and images

--- Estimator_Schema.sql
+++ Estimator_Schema.sql
@@ -1,26 +1,40 @@
--- Home Depot (Home Depot Product ID API)
-CREATE TABLE HomeDepot_table (
+CREATE TABLE cleaned_data_example (
+  link_id TEXT FOREIGN KEY NOT NULL,
   material_description VARCHAR (160),
+  product_id TEXT FOREIGN KEY,
+  material_price FLOAT FOREIGN KEY,
+  vendor_location TEXT FOREIGN KEY,
+  link VARCHAR (200),
+  formatted_datetime DATE
+);
+
+CREATE TABLE lowest_price_table (
+  product_id TEXT FOREIGN KEY,
+  material_description VARCHAR (160),
+  material_price FLOAT PRIMARY KEY NOT NULL,
+  vendor_location TEXT FOREIGN KEY,
+  link VARCHAR (200),
+  formatted_datetime DATE
+);
+
+CREATE TABLE material_description_table (
   product_id TEXT PRIMARY KEY NOT NULL,
-  material_price FLOAT,
-  vendor_location TEXT,
-  formatted_datetime DATE
-);
-
--- Cleaned_Data (google search results)
-CREATE TABLE Cleaned_Data (
   material_description VARCHAR (160),
-  material_price FLOAT,
-  vendor_location TEXT,
-  link VARCHAR (200),
   formatted_datetime DATE
 );
 
--- Working_Table (google search results)
-CREATE TABLE Working_Table (
-  material_mescription VARCHAR (160),
-  material_price FLOAT,
+CREATE TABLE all_vendor_table (
+  vendor_id TEXT PRIMARY KEY NOT NULL,
   vendor_location TEXT,
+  formatted_datetime DATE
+);
+
+CREATE TABLE all_link_table (
+  link_id TEXT PRIMARY KEY NOT NULL,
   link VARCHAR (200),
+  material_description VARCHAR (160) FOREIGN KEY,
+  material_price FLOAT,
+  product_id TEXT FOREIGN KEY,  
+  vendor_location TEXT FOREIGN KEY,  
   formatted_datetime DATE
 );
