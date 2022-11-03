2de27ad900402a2f8d4e397eae207ee847a359ba
thegreatkeej <106637080+thegreatkeej@users.noreply.github.com>
2022-11-02 17:10:07 -0700

    Daily data update, added images to README

--- Estimator_Schema.sql
+++ Estimator_Schema.sql
@@ -1,44 +1,40 @@
--- Cleaned_Data (google search results)
-CREATE TABLE cleaned_data (
+CREATE TABLE cleaned_data_example (
+  link_id TEXT FOREIGN KEY NOT NULL,
   material_description VARCHAR (160),
-  product_id TEXT,
-  material_price FLOAT,
-  vendor_location TEXT,
+  product_id TEXT FOREIGN KEY,
+  material_price FLOAT FOREIGN KEY,
+  vendor_location TEXT FOREIGN KEY,
   link VARCHAR (200),
   formatted_datetime DATE
 );
 
--- Working_Table (google search results)
-CREATE TABLE working_table (
+CREATE TABLE lowest_price_table (
+  product_id TEXT FOREIGN KEY,
   material_description VARCHAR (160),
-  product_id TEXT,
-  material_price FLOAT,
-  vendor_location TEXT,
+  material_price FLOAT PRIMARY KEY NOT NULL,
+  vendor_location TEXT FOREIGN KEY,
   link VARCHAR (200),
   formatted_datetime DATE
 );
 
--- Material_Description_Table (google search results)
 CREATE TABLE material_description_table (
   product_id TEXT PRIMARY KEY NOT NULL,
   material_description VARCHAR (160),
   formatted_datetime DATE
 );
 
--- All_Vendor_Table (google search results)
 CREATE TABLE all_vendor_table (
   vendor_id TEXT PRIMARY KEY NOT NULL,
   vendor_location TEXT,
   formatted_datetime DATE
 );
 
--- All_Link_Table (google search results)
 CREATE TABLE all_link_table (
   link_id TEXT PRIMARY KEY NOT NULL,
   link VARCHAR (200),
-  material_description VARCHAR (160),
+  material_description VARCHAR (160) FOREIGN KEY,
   material_price FLOAT,
-  product_id TEXT,  
-  vendor_location TEXT,  
+  product_id TEXT FOREIGN KEY,  
+  vendor_location TEXT FOREIGN KEY,  
   formatted_datetime DATE
 );
