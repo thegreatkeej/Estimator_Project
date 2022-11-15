1010885ce2332d75a9525b2b6fda003734c7caa3
alexei mendoza <amendoza416@icloud.com>
2022-11-14 19:29:11 -0800

    update

--- OrgDataFrames/concatenated_results.sql
+++ OrgDataFrames/concatenated_results.sql
@@ -2,14 +2,12 @@ SELECT * FROM clean_df
 SELECT * FROM material_description_table
 SELECT * FROM vendor_table
 
-SELECT material_description_table.*, clean_df.material_price,clean_df.vendor,clean_df.link
-FROM material_description_table inner join clean_df
-ON clean_df.formatted_datetime = material_description_table.formatted_datetime
-
 
 SELECT clean_df.*, material_description_table.product_id
 FROM clean_df INNER join material_description_table
-ON material_description_table.formatted_datetime = clean_df.formatted_datetime
+ON material_description_table.material_description = clean_df.material_description
 
 
-                                                          
+SELECT clean_df.*, vendor_table.location
+FROM clean_df INNER join vendor_table
+ON vendor_table.vendor = clean_df.vendor                                                         
